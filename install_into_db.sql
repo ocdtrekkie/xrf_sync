f655604bd1dff4ac93ef3cd63eb1dbe94c8f96fc
Jacob Weisz <inbox@jacobweisz.com>
2020-09-24 11:50:38 -0500

    Update database for message support

--- install_into_db.sql
+++ install_into_db.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS `y_messages` (
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `source` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of source node',
+  `dest` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of destination node',
+  `sent` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Date message sent',
+  `recv` int(1) NOT NULL DEFAULT '0' COMMENT '0 if unread, 1 if retrieved',
+  `mesg` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'Content of message',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Messages queued for delivery';
+
 CREATE TABLE IF NOT EXISTS `y_nodes` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
   `pool_id` int(4) NOT NULL DEFAULT '0' COMMENT 'Identifies a group of nodes which can communicate, 0 is no pool',
@@ -7,5 +17,7 @@ CREATE TABLE IF NOT EXISTS `y_nodes` (
   `last_seen` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last check in date',
   `last_ip_addr` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'IP address at last check in',
   `user_agent` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'User agent of node',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `descr` (`descr`),
+  UNIQUE KEY `access_key` (`access_key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Devices participating in sync module';
